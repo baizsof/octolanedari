7724f06c0cdfdc778d8b0b60cc0328fcde74f407
baizsof <balazs.izsof@gmail.com>
2023-01-16 18:29:44 +0100

    add calendari.calendar.configuration.GoogleCalendarConfiguration.Companion.fromFile

--- src/main/kotlin/calendari/calendar/configuration/GoogleCalendarConfiguration.kt
+++ src/main/kotlin/calendari/calendar/configuration/GoogleCalendarConfiguration.kt
@@ -1,18 +1,28 @@
 package calendari.calendar.configuration
 
+import java.io.File
 import java.net.URL
 import java.util.TimeZone
 
 class GoogleCalendarConfiguration(
-    private val id: String,
+    private val calendarId: String,
     private val apiKey: String,
     private val timeZone: String
 ) : CalendarConfiguration {
+
     override fun getBaseUrl(): URL {
-        return URL("https://clients6.google.com/calendar/v3/calendars/$id/events?&key=$apiKey")
+        return URL("https://clients6.google.com/calendar/v3/calendars/$calendarId/events?&key=$apiKey")
     }
 
     override fun getTimezone(): TimeZone {
         return TimeZone.getTimeZone(timeZone)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun fromFile(file : File) : GoogleCalendarConfiguration {
+            throw NotImplementedError()
+        }
     }
 }
