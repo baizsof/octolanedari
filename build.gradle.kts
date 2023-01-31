e4be3114483e4ed3ac9a92bb5c2604c37cd0cabd
baizsof <balazs.izsof@gmail.com>
2023-01-31 17:19:44 +0100

    rm unused dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,12 +14,10 @@ repositories {
 
 dependencies {
     implementation("org.apache.httpcomponents:httpclient:4.5.13")
-    implementation ("com.google.code.gson:gson:2.10")
     implementation("org.json:json:20220924")
     implementation("org.apache.commons:commons-lang3:3.12.0")
     implementation("joda-time:joda-time:2.12.2")
 
-    testImplementation("pl.pragmatists:JUnitParams:1.1.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     testImplementation(kotlin("test"))
 }
