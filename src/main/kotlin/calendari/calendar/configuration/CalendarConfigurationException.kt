c812ca092c94c9de205bb52019c84aaca9b00151
baizsof <balazs.izsof@gmail.com>
2023-01-31 16:49:49 +0100

    add CalendarConfigurationException msg constants

--- src/main/kotlin/calendari/calendar/configuration/CalendarConfigurationException.kt
+++ src/main/kotlin/calendari/calendar/configuration/CalendarConfigurationException.kt
@@ -1,4 +1,8 @@
 package calendari.calendar.configuration
 
 class CalendarConfigurationException(msg : String) : Exception(msg){
+    companion object {
+        val TIME_ZONE_REQUIRED = "Timezone is required for calendar configuration"
+        val CALENDAR_ID_REQUIRED = "Calendar id is required for calendar configuration"
+    }
 }
