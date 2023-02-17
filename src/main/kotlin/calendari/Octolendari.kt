4ea24914b51859d3c804dddabb6ef330215cf19e
baizsof <balazs.izsof@gmail.com>
2023-02-17 16:05:28 +0100

    basic impl for Main

--- src/main/kotlin/calendari/Octolendari.kt
+++ src/main/kotlin/calendari/Octolendari.kt
@@ -2,6 +2,11 @@ package calendari
 
 import calendari.args.OctolendariArgParser
 import calendari.args.OctolendariConfiguration
+import calendari.presenter.QueryPresenter
+import calendari.presenter.TxtFileQueryPresenter
+import calendari.query.Query
+import calendari.query.factory.OneToManyFreeOverlappingIntervalQueryFactory
+import calendari.query.factory.QueryFactory
 
 class Octolendari {
     companion object {
@@ -9,6 +14,11 @@ class Octolendari {
         @JvmStatic
         fun main(args: Array<String>) {
             val configuration: OctolendariConfiguration = OctolendariArgParser().parse(args)
+            val queryFactory : QueryFactory = OneToManyFreeOverlappingIntervalQueryFactory(configuration)
+            val query : Query = queryFactory.create()
+            val eventCandidates = query.doQuery()
+            val presenter : QueryPresenter = TxtFileQueryPresenter(configuration.output)
+            presenter.present(eventCandidates)
         }
     }
 }
