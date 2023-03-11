fda9b471bea831f4da57680c2daa9a4f89b3a001
baizsof <balazs.izsof@gmail.com>
2023-03-11 12:29:56 +0100

    OCT-3 implement GooglePrivateApiEventMapper

--- src/main/kotlin/calendari/calendar/mapper/google/GooglePrivateApiEventMapper.kt
+++ src/main/kotlin/calendari/calendar/mapper/google/GooglePrivateApiEventMapper.kt
@@ -2,9 +2,15 @@ package calendari.calendar.mapper.google
 
 import calendari.calendar.mapper.EventMapper
 import com.google.api.services.calendar.model.Event
+import org.joda.time.Interval
 
 class GooglePrivateApiEventMapper : EventMapper<Event> {
-    override fun getEvent(event : Event): calendari.calendar.Event {
-        TODO("Not yet implemented")
+    override fun getEvent(event: Event): calendari.calendar.Event {
+        val interval : Interval? = try {
+            Interval(event.start.dateTime.value, event.end.dateTime.value)
+        } catch (ex: NullPointerException) {
+            Interval(event.start.date.value, event.end.date.value)
+        }
+        return calendari.calendar.Event(interval!!, event.organizer.email)
     }
 }
