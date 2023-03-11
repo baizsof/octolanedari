4229e658107003895b6880a0b11b102d5f5daac2
baizsof <balazs.izsof@gmail.com>
2023-03-11 12:59:49 +0100

    OCT-3 implement calendari.calendar.configuration.google.GooglePrivateCalendarConfiguration.Companion.fromProperties

--- src/main/kotlin/calendari/calendar/configuration/google/GooglePrivateCalendarConfiguration.kt
+++ src/main/kotlin/calendari/calendar/configuration/google/GooglePrivateCalendarConfiguration.kt
@@ -1,10 +1,19 @@
 package calendari.calendar.configuration.google
 
 import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.Properties
 
 class GooglePrivateCalendarConfiguration(
-     val tokenPath: Path,
-     val credentialFilePath: Path,
+    val tokenPath: Path,
+    val credentialFilePath: Path,
 ) {
-
+    companion object {
+        @JvmStatic
+        fun fromProperties(properties: Properties): GooglePrivateCalendarConfiguration {
+            val tokenPath = Paths.get(properties.getProperty("token-path").toString())
+            val credentialFilePath = Paths.get(properties.getProperty("credential-file-path").toString())
+            return GooglePrivateCalendarConfiguration(tokenPath, credentialFilePath)
+        }
+    }
 }
